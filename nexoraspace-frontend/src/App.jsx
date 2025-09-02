f47fcb30419073886ef6e0cd7157264cf0336ba9
Git-Hitesh06 <hitesh06p@gmail.com>
2025-09-02 14:16:23 +0530

    Added routing setup

--- nexoraspace-frontend/src/App.jsx
+++ nexoraspace-frontend/src/App.jsx
@@ -1,23 +1,12 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SystemAdminDashboard from "./components/SystemAdminDashboard";
-import SystemAdminAddCompany from "./components/SystemAdminAddCompany";
-import HomePage from "./components/HomePage";
-import SystemAdminLogin from "./components/SystemAdminLogin";
-import SystemAdminCompanyProfile from "./components/SystemaAdminCompanyProfile";
+import AppRoutes from "./Routes/Routes";
 
 const App = () => {
   return (
-    <Router>
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/system/login" element={<SystemAdminLogin />} />
-        <Route path="/system/dashboard" element={<SystemAdminDashboard />} />
-        <Route path="/system/add-company" element={<SystemAdminAddCompany />} />
-        <Route path="/system/company-profile" element={<SystemAdminCompanyProfile />} />
-      </Routes>
-    </Router>
+    <div className="app">
+      <AppRoutes />
+    </div>
   );
 };
 
---
+++ nexoraspace-frontend/src/Routes/Routes.jsx
@@ -0,0 +1,22 @@
+// src/Routes/Routes.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SystemAdminDashboard from "../components/SystemAdminDashboard";
+import SystemAdminAddCompany from "../components/SystemAdminAddCompany";
+import HomePage from "../components/HomePage";
+import SystemAdminLogin from "../components/SystemAdminLogin";
+import SystemAdminCompanyProfile from "../components/SystemaAdminCompanyProfile";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/system/login" element={<SystemAdminLogin />} />
+      <Route path="/system/dashboard" element={<SystemAdminDashboard />} />
+      <Route path="/system/add-company" element={<SystemAdminAddCompany />} />
+      <Route path="/system/company-profile" element={<SystemAdminCompanyProfile />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
